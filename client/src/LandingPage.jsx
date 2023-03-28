e84c32e9f8b1860520188386b06121d699e0274e
stevenhorn17 <stevenhorn02@gmail.com>
2023-03-28 17:25:02 -0400

    landing page updates

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,19 +1,103 @@
 import './App.css';
 import { useGlobalState } from "./GlobalState.js";
+import { useState } from 'react';
+import axios from 'axios';  
+
 
 const LandingPage = () => {
   const [globalState, updateGlobalState] = useGlobalState();
+  const [publicEvents, setPublicEvents] = useState([]);
+  const [privateEvents, setPrivateEvents] = useState([]);
+  const [RSOEvents, setRSOEvents] = useState([]);
+
+  // useEffect(()=>{
+  //   getPrivateEvents();
+  //   getPublicEvents();
+  //   getRSOEvents();
+  // },[]);
+
+  const getPublicEvents = async() => {
+    axios.post(`http://localhost:8800/events/public`)
+      .then((response)=>{
+        setPublicEvents(response.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+
+  const getPrivateEvents = async() => {
+    axios.post(`http://localhost:8800/events/private`)
+      .then((response)=>{
+        setPublicEvents(response.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+  }
+
+  const getRSOEvents = async(RSO) => {
+    axios.post(`http://localhost:8800/events/rso`)
+      .then((response)=>{
+        setPublicEvents(response.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+  }
 
   return(
     <div>
     
-      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{position: 'absolute',top: "5%", left: "50%"}}>
         <p style={{fontWeight: 'bold'}}>Welcome {globalState.user.username}!</p>
       </div>
       <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
-        <div>Private Events</div>
-        <div>Public Events</div>
-        <div>RSO Events</div>
+        <div><h1 style={{position: 'absolute', top: "10%", left: '5%'}}>Private Events</h1></div>
+          <ul className='manage-private-events-list'>
+            {
+              privateEvents.length > 0 ? privateEvents.map(item=>
+                <li key={item.id} value={item.id}>
+                  {<div className='manage-uni-card'>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <p>{item.location}</p>
+                  <button type="button" value={item.id} className='manage-uni-delete' onClick={(e) => deleteUni(e.target.value)}>Delete</button>
+                </div>}
+                </li>
+                ) : <li><h1>No Private Events to see ... try making some!</h1></li>
+            }
+          </ul>
+        <div><h1 style={{position: 'absolute', top: "10%", right: "40%"}}>Public Events</h1></div>
+          <ul className='manage-public-events-list'>
+            {
+              publicEvents.length > 0 ? publicEvents.map(item=>
+                <li key={item.id} value={item.id}>
+                  {<div className='manage-uni-card'>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <p>{item.location}</p>
+                  <button type="button" value={item.id} className='manage-uni-delete' onClick={(e) => deleteUni(e.target.value)}>Delete</button>
+                </div>}
+                </li>
+                ) : <li><h1>No Public to see ... try making some!</h1></li>
+            }
+        </ul>
+        <div><h1 style={{position: 'absolute', top: "10%", right: '5%'}}>Private Events</h1></div>
+          <ul className='manage-rso-events-list'>
+            {
+              RSOEvents.length > 0 ? RSOEvents.map(item=>
+                <li key={item.id} value={item.id}>
+                  {<div className='manage-uni-card'>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <p>{item.location}</p>
+                  <button type="button" value={item.id} className='manage-uni-delete' onClick={(e) => deleteUni(e.target.value)}>Delete</button>
+                </div>}
+                </li>
+                ) : <li><h1>No RSO Events to see ... try making some!</h1></li>
+            }
+        </ul>
       </div>
 
     </div>
