4be358f9a843d27c7dee68cec01b192678e6de6e
stevenhorn17 <stevenhorn02@gmail.com>
2023-04-02 15:17:10 -0400

    Fix Grid layout issue & other smaller stuff

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -21,25 +21,28 @@ const LandingPage = () => {
     getAllRatings();
   }, []);
 
-  const EventInfo = ({ event , onClose}) => {
+  const EventInfo = ({ event , onClose, comments}) => {
     return (
       <div className='event-info-modal'>
         <div className='event-info-content'>
           <button className='event-info-close' onClick={onClose}>X</button>
-            <h1>Event Name: {event?.name}</h1>
-            <p>Category: {event?.category}</p>
-            <p>Description: {event?.description}</p>
-            <p>Time: {event?.time}</p>
-            <p>Date: {event?.date}</p>
-            <p>Location ID: {event?.location_id}</p>
-            <p>Location: {event?.location}</p>
-            <p>Phone: {event?.phone}</p>
-            <p>Email: {event?.email}</p>
-            <p>Status: {event?.status}</p>
-            <p>Uni ID: {event?.uni_id}</p>
-            <p>RSO ID: {event?.rso_id}</p>
-            <p>Admin ID: {event?.admin_id}</p>
-            </div>
+          <h2>Event Name: {event?.name}</h2>
+          <p>Category: {event?.category}</p>
+          <p>Description: {event?.description}</p>
+          <p>Time: {event?.time}</p>
+          <p>Date: {event?.date}</p>
+          <p>Location ID: {event?.location_id}</p>
+          <p>Location: {event?.location}</p>
+          <p>Phone: {event?.phone}</p>
+          <p>Email: {event?.email}</p>
+          <p>Status: {event?.status}</p>
+          <p>Uni ID: {event?.uni_id}</p>
+          <p>RSO ID: {event?.rso_id}</p>
+          <p>Admin ID: {event?.admin_id}</p>
+          <div>
+            <p>{comments}</p>
+          </div>
+        </div>
       </div>
     );
   };
@@ -115,10 +118,12 @@ const LandingPage = () => {
     const allEvents = [...privateEvents, ...publicEvents, ...RSOEvents];
     const event = allEvents.find((e) => e.id === parseInt(eventID, 10));
     setSelectedEvent(event);
+    // Set comments & ratings for event?
   }
 
   function closeEvent() {
     setSelectedEvent(null);
+    // Undo set comment & rating
   }
 
   return(
@@ -128,13 +133,13 @@ const LandingPage = () => {
         <p style={{fontWeight: 'bold'}}>Welcome {globalState.user.username}!</p>
       </div>
       <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
-        <div><h1 style={{position: 'absolute', top: "10%", left: '5%'}}>Private Events</h1></div>
+        <div><h1 style={{position: 'absolute', top: "10%", left: '5%'}}>Private Events</h1>
           <ul className='manage-private-events-list'>
             {
               privateEvents.length > 0 ? privateEvents.map(item=>
                 <li key={item.id} value={item.id}>
                   {<div className='manage-event-card'>
-                  <h2>{item.name}</h2>
+                  <h2>Event: {item.name}</h2>
                   <p>{item.description}</p>
                   <p>{item.location}</p>
                   <button type="button" value={item.id} className='manage-event-open'
@@ -144,15 +149,17 @@ const LandingPage = () => {
                 ) : <li><h1>No Private Events to see ... try making some!</h1></li>
             }
           </ul>
-        <div><h1 style={{position: 'absolute', top: "10%", right: "40%"}}>Public Events</h1></div>
+        </div>
+        <div><h1 style={{position: 'absolute', top: "10%", right: "40%"}}>Public Events</h1>
           <ul className='manage-public-events-list'>
             {
               publicEvents.length > 0 ? publicEvents.map(item=>
                 <li key={item.id} value={item.id}>
                   {<div className='manage-event-card'>
-                  <h2>{item.name}</h2>
+                  <h2>Event: {item.name}</h2>
                   <p>{item.description}</p>
                   <p>{item.location}</p>
+                  <p>Comments: {allComments}</p>
                   <button type="button" value={item.id} className='manage-event-open' 
                   onClick={(e) => openEvent(e.target.value)}>Event Info</button>
                 </div>}
@@ -160,13 +167,14 @@ const LandingPage = () => {
                 ) : <li><h1>No Public to see ... try making some!</h1></li>
             }
         </ul>
-        <div><h1 style={{position: 'absolute', top: "10%", right: '5%'}}>Private Events</h1></div>
+        </div>
+        <div><h1 style={{position: 'absolute', top: "10%", right: '5%'}}>RSO Events</h1>
           <ul className='manage-rso-events-list'>
             {
               RSOEvents.length > 0 ? RSOEvents.map(item=>
                 <li key={item.id} value={item.id}>
                   {<div className='manage-event-card'>
-                  <h2>{item.name}</h2>
+                  <h2>Event: {item.name}</h2>
                   <p>{item.description}</p>
                   <p>{item.location}</p>
                   <button type="button" value={item.id} className='manage-event-open' 
@@ -177,6 +185,7 @@ const LandingPage = () => {
             }
         </ul>
       </div>
+    </div>
       {
         selectedEvent && <EventInfo event={selectedEvent} onClose={closeEvent}/>
       }
